86522119a00706fa14b92c293fad7e41a1686315
Jonah Brenner <jonahbrenner@dhcp-10-250-196-173.harvard.edu>
2021-11-30 00:49:58 -0500

    Add existing file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template, Response
+from flask import Flask, render_template, Response, request, redirect
+from werkzeug.utils import secure_filename
 import sys
+import os
 # Tornado web server
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
@@ -39,6 +41,36 @@ def show_entries():
     general_Data = {
         'title': 'Music Player'}
     return render_template('simple.html')
+    
+
+# Upload mp3 file
+app.config['UPLOAD_FOLDER'] = 'music/uploads'
+app.config['MAX_CONTENT_PATH'] = 1000000000 # bytes (arbitrary for now)
+ALLOWED_EXTENSIONS = {'mp3','mp4'} # add to this
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect('/') #unsure what the url would be? test
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect('/')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/')
+    return "success!"
+
 
 #launch a Tornado server with HTTPServer.
 if __name__ == "__main__":
